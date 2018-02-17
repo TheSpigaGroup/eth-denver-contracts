88ef41206e31eaacd3c3f00fb17e2991b6102388
Greg Perrin <greg.perrin@mfactorengineering.com>
2018-02-17 00:55:40 -0700

    [TESTING]-- Build commands, contract dev.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Import libraries
 const Web3 = require('web3');
+const BigNumber = require('bignumber.js');
 const contract = require('truffle-contract');
 const Token = require('./build/contracts/Token.json');
+const keys = require('./keys.json');
 
 // Setup RPC connection
-const provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
 
 // Read JSON and attach RPC connection (Provider)
 const token = contract(Token);
@@ -16,8 +18,8 @@ token.currentProvider.sendAsync = function () {
 
 // Use Truffle as usual
 token.deployed().then(instance => {
-  console.log(instance);
-  return instance;
+  console.log(Object.keys(keys.addresses)[0]);
+  return instance.startCustody(Object.keys(keys.addresses)[0].split('x')[1], new BigNumber("2134"));
 }).then(result => {
   console.log(result);
 }).catch(err => {
